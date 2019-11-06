78fef0350aa1e81b28578237cbdf74c5dae0aa92
Yodsakorn009 <meanyok.mg@gmail.com>
2019-11-06 22:07:33 +0700

    game

--- server.js
+++ server.js
@@ -14,59 +14,60 @@ app.get('/', function (req, res) {
   res.render('pages/index');
 });
 
-app.get('/addmusic', function (req, res) {
-  res.render('pages/addmusic');
+app.get('/addgame', function (req, res) {
+  res.render('pages/addgame');
 });
 
-app.get('/music', function (req, res) {
+app.get('/game', function (req, res) {
   MongoClient.connect(url, options, function (err, db) {
     if (err) throw err;
-    var dbo = db.db("Musics");
+    var dbo = db.db("game");
     var query = {};
-    dbo.collection("Musicsdb")
+    dbo.collection("game")
       .find(query).toArray(function (err, result) {
         if (err) throw err;
         console.log(result);
-        res.render('pages/music', { music: result });
+        res.render('pages/game', { game: result });
         db.close();
       });
   });
 
 });
 
-app.post('/addmusicadd', function (req, res) {
+app.post('/addgameadd', function (req, res) {
   var names = req.body.name;
-  var albums= req.body.album;
+  var types= req.body.type;
+  var rates = req.body.rate;
   var prices = req.body.price;
-  var dates = req.body.date;
   MongoClient.connect(url, function(err, db) {
     if (err) throw err;
-    var dbo = db.db("Musics");
-    var myobj = {  music_name: names,
-      album: albums,
-      price: prices,
-      date:dates
+    var dbo = db.db("game");
+    var myobj = {  
+      Name: names,
+      Type: types,
+      Rate: rates,
+      Price:prices
      };
-    dbo.collection("Musicsdb").insertOne(myobj, function(err, result) {
+    dbo.collection("game").insertOne(myobj, function(err, result) {
       if (err) throw err;
       console.log("1 document inserted");
       db.close();
-      res.redirect("/music");
+      res.redirect("/game");
     });
   });
 
 });
 
-app.get('/musicdetails/:name', function (req, res) {
+app.get('/gamedetails/:name', function (req, res) {
   var namemus = req.params.name;
   MongoClient.connect(url, options, function (err, db) {
     if (err) throw err;
-    var dbo = db.db("Musics");
-    var query = { music_name: namemus };
-    dbo.collection("Musicsdb").findOne(query, function (err, result) {
+    var dbo = db.db("game");
+    var query = { Name: namemus };
+    dbo.collection("game").findOne(query, function (err, result) {
       if (err) throw err;
       console.log(result);
-      res.render('pages/musicdetails', { detail: result });
+      res.render('pages/gamedetails', { detail: result });
       db.close();
     });
   });
@@ -76,9 +77,9 @@ app.get('/edit/:name', function (req, res) {
   var namemusedit = req.params.name;
   MongoClient.connect(url, options, function (err, db) {
     if (err) throw err;
-    var dbo = db.db("Musics");
-    var query = { music_name: namemusedit };
-    dbo.collection("Musicsdb").findOne(query, function (err, result) {
+    var dbo = db.db("game");
+    var query = { Name: namemusedit };
+    dbo.collection("game").findOne(query, function (err, result) {
       if (err) throw err;
       console.log(result);
       res.render('pages/edit', { detail: result });
@@ -87,28 +88,28 @@ app.get('/edit/:name', function (req, res) {
   });
 });
 
-app.post('/musicsave', function (req, res) {
+app.post('/gamesave', function (req, res) {
   var names = req.body.name;
-  var albums= req.body.album;
+  var types= req.body.type;
+  var rates = req.body.rate;
   var prices = req.body.price;
-  var dates = req.body.date;
   MongoClient.connect(url, options, function (err, db) {
     if (err) throw err;
-    var dbo = db.db("Musics");
-    var myquery = { music_name: names };
+    var dbo = db.db("game");
+    var myquery = { Name: names };
     var newvalues = {
       $set: {
-        music_name: names,
-        album: albums,
-        price: prices,
-        date:dates
+        Name: names,
+        Type: types,
+        Rate: rates,
+        Price:prices
       }
     };
-    dbo.collection("Musicsdb").updateOne(myquery, newvalues, function (err, result) {
+    dbo.collection("game").updateOne(myquery, newvalues, function (err, result) {
       if (err) throw err;
       console.log("1 document updated");
       db.close();
-      res.redirect("/music");
+      res.redirect("/game");
     });
   });
 });
@@ -117,14 +118,13 @@ app.get('/delete/:name', function (req, res) {
   var named = req.params.name;
   MongoClient.connect(url, function(err, db) {
     if (err) throw err;
-    var dbo = db.db("Musics");
-    var myquery = {music_name:
-      named};
-    dbo.collection("Musicsdb").deleteOne(myquery, function(err, obj) {
+    var dbo = db.db("game");
+    var myquery = { Name:named};
+    dbo.collection("game").deleteOne(myquery, function(err, obj) {
       if (err) throw err;
       console.log("1 document deleted");
       db.close();
-      res.redirect("/music");
+      res.redirect("/game");
     });
   });
 });
